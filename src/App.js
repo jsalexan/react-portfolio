5083d6f0b816baca73e1e4148cf5f682749aa338
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-01-12 14:53:37 -0800

    Restructured the files a bit and added the header and footer. Need to work on the other pages and redo all the styling. Next try the form?

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 
 function App() {
     return (
       <div>
-        <PortfolioContainer />
+        
+        <Header />
+
         <Footer />
       </div>
     );
