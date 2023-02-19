293e4e028b69fc6a1a35062023bf1208e9272523
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-02-18 18:06:32 -0800

    Fixed the footer and changed some of the text colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,37 +3,42 @@ import React from "react";
 function Footer() {
   return (
     <footer className="footer">
-    
-        <nav className="contact-me-container" id="contact-me">
-            <ul>
-                <li>
-                    <a href="tel:1-609-240-3935">
-                    <i className="fa-solid fa-phone fa-2xl"></i></a>
-                </li>
-                <li>
-                    <a href="mailto:jsa.wa.us@gmail.com">
-                    <i className="fa-solid fa-envelope fa-2xl"></i></a>
-                </li>
-                <li>
-                    <a href="https://github.com/jsalexan">
-                    <i className="fa-brands fa-github fa-2xl"></i></a>
-                </li>
-                <li>
-                    <a href="https://www.linkedin.com/in/jennifer-alexander-hill/">
-                    <i className="fa-brands fa-linkedin fa-2xl"></i></a>
-                </li>
-                <li>
-                    <a href="https://twitter.com/JSAH2000">
-                    <i className="fa-brands fa-twitter fa-2xl"></i></a>
-                </li>
-                
-            </ul>
-        </nav>
+      <nav className="fixed-bottom" id="contact-me">
+        <div className='contact-me-container' >
+        <ul>
+          <li>
+            <a href="tel:1-609-240-3935">
+              <i className="fa-solid fa-phone fa-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a href="mailto:jsa.wa.us@gmail.com">
+              <i className="fa-solid fa-envelope fa-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/jsalexan">
+              <i className="fa-brands fa-github fa-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/jennifer-alexander-hill/">
+              <i className="fa-brands fa-linkedin fa-2xl"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/JSAH2000">
+              <i className="fa-brands fa-twitter fa-2xl"></i>
+            </a>
+          </li>
+        </ul>
+        </div>
+        <div>
+          <h3>JAH Designs 2023</h3>
+        </div>
+      </nav>
 
-    <div>
-        <h3>JAH Designs 2023</h3>
-    </div>
-</footer>
-)
-};
+    </footer>
+  );
+}
 export default Footer;
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,7 +7,6 @@ export default function Contact() {
   return (
     <div className='contact-page-container'>
     <div className='contact-container'>
-        <h2>Contact Me</h2>
         <p className="final-thoughts">Thank you so much for visiting! 
                         <br /><br />Please leave me a note to share your thoughts.</p>
         <Form />       
