a2908427c04b99562cc520433a1d0bfaef0ea787
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-03-02 15:14:47 -0800

    Added some padding to bottom of contact page to fix button visibility on mobile. Then updated top margins, as well as button width.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
     <div className='contact-page-container'>
     <div className='contact-container'>
         <p className="final-thoughts">Thank you so much for visiting! 
-                        <br /><br />Please leave me a note to share your thoughts.</p>
+                        <br />Why not drop me a note?</p>
         <Form />       
     </div>
     </div>
