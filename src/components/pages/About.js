73dce32886fc8bc812e4546c6a376db63bf53896
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-10-06 14:24:42 -0700

    Fixed some hover CSS. Added hover feature to highlight h4 words in About

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import profilePic from '../../assets/polaroid.png';
 
 export default function About() {
+  
+    const [highlightedWord, setHighlightedWord] = useState(null);
+  
+    const handleWordHover = (word) => {
+      setHighlightedWord(word);
+    };
+  
+    const handleWordLeave = () => {
+      setHighlightedWord(null);
+    };
   return (
     <section id="about-me">
         <header className='top-bar'>
@@ -10,7 +20,29 @@ export default function About() {
             <div className="about-me-container">
                 <img className="polaroid" src={ profilePic } alt="Jennifer Alexander-Hill, a woman with brown hair."/>
                     <p>
-                        <b>Study. Simplify. Solve.</b>
+                    <h4 className='heading'>
+      <span
+        className={highlightedWord === 'Study' ? 'highlighted' : ''}
+        onMouseEnter={() => handleWordHover('Study')}
+        onMouseLeave={handleWordLeave}
+      >
+        Study.
+      </span>{' '}
+      <span
+        className={highlightedWord === 'Simplify' ? 'highlighted' : ''}
+        onMouseEnter={() => handleWordHover('Simplify')}
+        onMouseLeave={handleWordLeave}
+      >
+        Simplify.
+      </span>{' '}
+      <span
+        className={highlightedWord === 'Solve' ? 'highlighted' : ''}
+        onMouseEnter={() => handleWordHover('Solve')}
+        onMouseLeave={handleWordLeave}
+      >
+        Solve.
+      </span>
+    </h4>
                         <br /><br />
                         Jennifer Alexander-Hill is a problem-solver. Whether its developing fundraising CRMs for nonprofit organizations, or – as she finds herself these days – buried in a code refactoring, Jennifer excels in creatively exploring the big picture and then diving deep into the details to help you reach your goals. 
                         <br /><br />
