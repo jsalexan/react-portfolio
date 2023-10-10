41060aae631769f7e2c3c9577b2d0ea6d065f4e2
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-10-10 15:36:01 -0700

    Updated the meta tags on the news. Worked on responsiveness. Revisit this.

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -13,9 +13,9 @@ export default function News() {
                 <div id="news-right">
                     <div className="update1">
                         <h3 className="news-headline">New Taylor Swift Countdown!</h3>
-                        <p className="news-text">In repsonse to all of the excitement around the Era's tour, I've created this simple countdown app. Select the concert you're attending, then count the seconds until the big event.
-                                                    
-                                                    Had a lot of fun figuring out the countdown mechanism, as well as adding some js to determine "minutes" vs "minute". Also, played with the Spotify API for the first time. Last, I incorporated a really fun confetti feature from a tutorial. Check it out! -JAH 10/10/2023</p>
+                        <p className="news-text">In response to all the excitement around the Era's tour, I've created this simple countdown app. Select the concert you're attending, then count the seconds until the big event.
+                                                                                
+                                                                            Had a lot of fun figuring out the countdown mechanism, as well as adding some js to determine "minutes" vs "minute". Also, played with the Spotify API for the first time. Last, I incorporated a really fun confetti feature from a tutorial. Check it out!</p><br/><span className="meta">10/10/2023</span>
                     </div>
                 </div>
 
@@ -29,7 +29,7 @@ export default function News() {
                 <div id="news-right">
                     <div className="update1">
                         <h3 className="news-headline">WordPress Freelancing</h3>
-                        <p className="news-text">Over the summer, I was contracted to redesign the website for a PTA group in West Seattle. I hadn't worked with WordPress in a number of years, so it was a fun break. The site will be maintained by numerous people, with limited to no tech background, so it was important to keep that in mind during development and help them achieve their vision. Take a look at the site! There have been some changes since I turned it over, but you can get a sense of the project. -JAH 09/09/2023</p>
+                        <p className="news-text">Over the summer, I was contracted to redesign the website for a PTA group in West Seattle. I hadn't worked with WordPress in a number of years, so it was a fun break. The site will be maintained by numerous people, with limited to no tech background, so it was important to keep that in mind during development, while helping them achieve their vision. Take a look at the site! There have been some changes since I turned it over, but you can get a sense of the project.</p><br/><span className="meta">09/09/2023</span>
                     </div>
                 </div>
             </div>
