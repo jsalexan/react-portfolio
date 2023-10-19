f277ea8b389fbcc670dfe4cc347ee50a55c8e631
Jennifer Alexander <110498167+jsalexan@users.noreply.github.com>
2023-10-19 16:24:46 -0700

    Added news about recent Chrome extension project

--- src/components/pages/News.js
+++ src/components/pages/News.js
@@ -6,6 +6,20 @@ export default function News() {
             <h1 className="news-heading">What I've Been Up To...</h1>
             <div className="news-container">
                 <div id="news-left">
+                    <a href="https://github.com/jsalexan/chrome-ext-concerts">
+                        <img className="news-image" src="https://res.cloudinary.com/dlxjksvbc/image/upload/v1697757536/Chrome_-circle_fiv2tj.png" alt="Two colorful wings"></img>
+                    </a>
+                </div>
+                <div id="news-right">
+                    <div className="update1">
+                        <h3 className="news-headline">Upcoming Concerts Chrome Extension</h3>
+                        <p className="news-text">This was a quick project, but really interesting to learn. With the help of a <a href="https://www.youtube.com/watch?v=B8Ihv3xsWYs">tutorial</a> and Rapid API, I created my own Chrome extension for viewing upcoming concerts in my area. To personalize it, I added JavaScript to show the user concerts 30 days out from today's date,included code to show and reformat the date of the event, and added pictures. Last I added CSS to style the data. This won't be my last extension. Very cool!
+</p><br/><span className="meta">10/19/2023</span>
+                    </div>
+                </div>
+            </div>
+            <div className="news-container">
+                <div id="news-left">
                     <a href="https://jsalexan.github.io/tay-clock/">
                         <img className="news-image" src="https://res.cloudinary.com/dlxjksvbc/image/upload/v1696973508/Taylor_Circle_rqzbti.png" alt="Taylor Swift concert poster"></img>
                     </a>
@@ -15,7 +29,7 @@ export default function News() {
                         <h3 className="news-headline">New Taylor Swift Countdown!</h3>
                         <p className="news-text">In response to all the excitement around the Era's tour, I've created this simple countdown app. Select the concert you're attending, then count the seconds until the big event.
                                                                                 
-                                                                            Had a lot of fun figuring out the countdown mechanism, as well as adding some js to determine "minutes" vs "minute". Also, played with the Spotify API for the first time. Last, I incorporated a really fun confetti feature from a tutorial. Check it out!</p><br/><span className="meta">10/10/2023</span>
+                                                                            Had a lot of fun figuring out the countdown mechanism, as well as adding some js to determine "minutes" vs "minute". Also, played with the Spotify API for the first time. Last, I incorporated a really fun confetti feature from a CSS tutorial. Check it out!</p><br/><span className="meta">10/10/2023</span>
                     </div>
                 </div>
 
